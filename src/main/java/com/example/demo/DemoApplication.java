9967f2de8155d97455819d00f46c77b52855c7a4
Jon Millington-Hotze <jonathan.mh@hotmail.co.uk>
2019-04-25 16:58:09 +0100

    cntroller added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Controller
+	public class SimpleController {
+		@Value("${spring.application.name}")
+		String appName;
+
+		@GetMapping("/")
+		public String homePage(Model model) {
+			model.addAttribute("appName", appName);
+			return "home";
+		}
 	}
 
 }
